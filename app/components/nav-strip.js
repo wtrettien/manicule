a9b57071be3e6a7486936e2a6708624d65ec95e7
Liza Daly <lizadaly@gmail.com>
2018-03-04 17:32:19 -0500

    [#1] Allow scrolling through the filmstrip

--- app/components/nav-strip.js
+++ app/components/nav-strip.js
@@ -1,57 +1,57 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import $ from 'jquery'
 
 import { Well, OverlayTrigger, Tooltip, Button, ButtonGroup } from 'react-bootstrap'
 
-const NavStrip = ({ data, setPage, currentPage, window }) => {
-  let before
-  let after
+class NavStrip extends React.Component {
 
-  // If "window" is -1, then the window is infinite and we assume that the UI will handle
-  // constraining the number of visible spreads.
-  if (window === -1) {
-    before = Array.from({ length: currentPage - 1 }, (v, k) => data[currentPage - k - 1]).reverse()
-    after = Array.from({ length: data.length - currentPage - 2 }, (v, k) => data[currentPage + 2 + k])
-  } else {
-    // Before window is `window` unless we're less than window
-    const beforeSlots = currentPage <= window ? currentPage - 1 : window
+  constructor(props) {
+    super(props)
+    const { currentPage, data } = props
 
-    // After window is `window` unless we're near the end
-    const afterSlots = currentPage + window > data.length ? data.length - currentPage : window
+    const before = Array.from({ length: currentPage - 1 }, (v, k) => data[currentPage - k - 1]).reverse()
+    const after = Array.from({ length: data.length - currentPage - 2 }, (v, k) => data[currentPage + 2 + k])
 
-    before = Array.from({ length: beforeSlots }, (v, k) => data[currentPage - k - 1]).reverse()
-    after = Array.from({ length: afterSlots }, (v, k) => data[currentPage + 2 + k])
+    const current = [data[currentPage], data[currentPage + 1]]
+    const items = before.concat(current).concat(after)
+    this.state = {
+      items,
+      current,
+    }
   }
-  const current = [data[currentPage], data[currentPage + 1]]
-  const items = before.concat(current).concat(after)
-
-  return (<Well bsClass="nav-group">
-    <NavGroup data={items} setPage={setPage} currentPage={currentPage} />
-    {/* <NavGroup data={before} setPage={setPage} />
-    <NavGroup data={[data[currentPage], data[currentPage + 1]]} setPage={setPage} isCurrent />
-    <NavGroup data={after} setPage={setPage} /> */}
-  </Well>
-  )
+  componentDidUpdate() {
+    if (this.props.currentPage > 9) {
+      const offset = $(`.thumbnail-${this.props.currentPage + 1}`).position().left - (($(window).width() / 2) - 90)
+      $('.nav-group').scrollLeft(offset)
+    }
+  }
+  render() {
+    return (<Well bsClass="nav-group">
+      <NavGroup data={this.state.items} setPage={this.props.setPage} currentPage={this.props.currentPage} />
+    </Well>
+    )
+  }
 }
+
 NavStrip.propTypes = {
   data: PropTypes.array.isRequired,
   setPage: PropTypes.func.isRequired,
   currentPage: PropTypes.number,
-  window: PropTypes.number,
 }
 NavStrip.defaultProps = {
   currentPage: 10,
   window: -1, // The length of the window of pages before/after. -1 is infinite
 }
 
-const NavGroup = ({ data, setPage, currentPage }) =>
-  (<ButtonGroup>{data.map((p) => {
+const NavGroup = ({ data, setPage, currentPage }) => (<ButtonGroup >
+  {data.map((p) => {
     const page = parseInt(p.index, 10)
     const pos = page % 2 === 0 ? 'recto' : 'verso'
     const pad = (`0000${p.index}`).substr(-4, 4)
     const edition = 'penn' // FIXME make this something switchable
     const img = require(`../images/book/${edition}/thumbnails/${pad}.jpg`) // eslint-disable-line global-require
-    const cls = `nav-thumbnail ${pos} ${page === currentPage || page === currentPage + 1 ? 'is-current' : ''}`
+    const cls = `nav-thumbnail thumbnail-${p.index} ${pos} ${page === currentPage || page === currentPage + 1 ? 'is-current' : ''}`
 
     return (<OverlayTrigger key={p.index} placement="top" overlay={<Tooltip id={p.index}>{p.signatures} - {p.category}</Tooltip>}>
       <div
@@ -60,7 +60,6 @@ const NavGroup = ({ data, setPage, currentPage }) =>
       >
         <Button
           style={{ backgroundImage: `url(${img})`,
-
           }}
           onClick={() => setPage(parseInt(p.index, 10))}
         >
@@ -69,7 +68,7 @@ const NavGroup = ({ data, setPage, currentPage }) =>
 
     </OverlayTrigger>)
   })}
-  </ButtonGroup>)
+</ButtonGroup>)
 
 
 NavGroup.propTypes = {
