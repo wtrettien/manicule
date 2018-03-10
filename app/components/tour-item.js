31a1be85c48e97a52a702de0a545635eb38e33ec
Liza Daly <lizadaly@gmail.com>
2018-03-10 16:14:28 -0500

    Add tour items

--- app/components/tour-item.js
+++ app/components/tour-item.js
@@ -3,18 +3,53 @@
 // Tour components
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Panel, Grid, Row, Col, Image } from 'react-bootstrap'
 
-const TourItem = ({ page, edition }) => {
+const TourItem = ({ page, edition, images }) => {
   // Get the HTML for this
   const html = require(`../tour/${edition}/${page}.html`) // eslint-disable-line global-require
   return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-    </div>
+    <Panel bsClass="tour">
+      <Grid>
+        <Row>
+          <Col sm={4}>
+            <TourImages images={images} edition={edition} />
+          </Col>
+          <Col sm={8}>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+          </Col>
+        </Row>
+      </Grid>
+    </Panel>
   )
 }
 TourItem.propTypes = {
   page: PropTypes.string.isRequired,
+  edition: PropTypes.string.isRequired,
+  images: PropTypes.array.isRequired,
+}
+
+
+const TourImages = ({ edition, images }) =>
+  // Get all the images for this page
+   (
+     <div>
+       {images.map((filename) => <TourImage key={filename} edition={edition} filename={filename} />)}
+     </div>
+  )
+
+
+TourImages.propTypes = {
+  images: PropTypes.array.isRequired,
+  edition: PropTypes.string.isRequired,
+}
+
+const TourImage = ({ filename, edition }) => {
+  const img = require(`../tour/${edition}/images/${filename}`)  // eslint-disable-line global-require
+  return <Image responsive src={img} alt="" />
+}
+TourImage.propTypes = {
+  filename: PropTypes.string.isRequired,
   edition: PropTypes.string.isRequired,
 }
 
--- app/containers/Tour/index.js
+++ app/containers/Tour/index.js
@@ -5,18 +5,20 @@ import { Grid, Row, Col } from 'react-bootstrap'
 import TourItem from '../../components/tour-item'
 import SiteContainer from '../SiteContainer'
 
+const Tour = ({ edition }) => {
+  const tourData = require(`../../tour/${edition}/tour.json`)   // eslint-disable-line global-require
+  const { page, images } = tourData[0]  // FIXME state management
 
-const Tour = ({ edition }) => (
-  <SiteContainer>
+  return (<SiteContainer>
     <Grid>
       <Row>
         <Col>
-          <TourItem page={7} edition={edition} />
+          <TourItem page={page} edition={edition} images={images} />
         </Col>
       </Row>
     </Grid>
-  </SiteContainer>
-  )
+  </SiteContainer>)
+}
 Tour.propTypes = {
   edition: PropTypes.string.isRequired,
 }
