cd9fa1751a90296c10fd42ff7c56fd639a721a09
Liza Daly <lizadaly@gmail.com>
2018-03-27 09:21:37 -0400

    [#19] Deploy from Travis on build

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-npm run build 
-rsync -az build/* liza@45.55.252.134:used-books/
+npm run build
+rsync -az build/* $USED_BOOKS_USER@$USED_BOOKS_HOST:used-books/
 
 resp=$(curl -X DELETE "https://api.cloudflare.com/client/v4/zones/2b3dcf5abc99772b1cccb4c5b5d13e04/purge_cache" \
-     -H "X-Auth-Email: lizadaly@gmail.com" \
+     -H "X-Auth-Email: $USED_BOOKS_EMAIL" \
      -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
      -H "Content-Type: application/json" \
      --data '{"purge_everything":true}' \
