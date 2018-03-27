b1e346740fbc9d53188b2f4a78448c6570696dc2
Liza Daly <lizadaly@gmail.com>
2018-03-27 10:19:04 -0400

    Give Travis some credentials

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 npm run build
-rsync -az build/* $USED_BOOKS_USER@$USED_BOOKS_HOST:used-books/
+rsync -az -e "ssh -i $HOME/.ssh/used-books-deploy" build/* $USED_BOOKS_USER@$USED_BOOKS_HOST:used-books/
+
+echo "Done deploying code, expiring CloudFlare cache..."
 
 resp=$(curl -X DELETE "https://api.cloudflare.com/client/v4/zones/2b3dcf5abc99772b1cccb4c5b5d13e04/purge_cache" \
      -H "X-Auth-Email: $USED_BOOKS_EMAIL" \
@@ -10,4 +12,4 @@ resp=$(curl -X DELETE "https://api.cloudflare.com/client/v4/zones/2b3dcf5abc9977
      --data '{"purge_everything":true}' \
      --write-out %{http_code} --silent --output /dev/null)
 
-echo "Status: $resp"
+echo "CloudFlare status: $resp"
