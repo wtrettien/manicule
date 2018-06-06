5bc3789c1feac5ee4d86262b83365b174aaa9da9
Liza Daly <lizadaly@gmail.com>
2018-06-05 20:17:04 -0400

    [#24] Add filmstrip navigation

--- app/components/nav-strip.js
+++ app/components/nav-strip.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import $ from 'jquery'
+import { Link } from 'react-router-dom'
 
-import { Well, ButtonGroup } from 'react-bootstrap'
+import { Well, ButtonGroup, Button } from 'react-bootstrap'
 import { getPageData } from '../utils/metadata'
 
 import Thumbnail from './thumbnail'
 
 
 class NavStrip extends React.Component {
-
   constructor(props) {
     super(props)
     const { currentPage, edition } = props
@@ -22,19 +22,23 @@ class NavStrip extends React.Component {
     const items = before.concat(current).concat(after)
     this.state = {
       items,
-      current,
     }
   }
   componentDidUpdate() {
     // Take the natural horizontal position of the current page element...
     let offset = $(`.thumbnail-${this.props.currentPage + 1}`).position().left
-    offset -= ($('.nav-group').width() / 2)  // divide the current filmstrip in half
+    offset -= ($('.nav-group').width() / 2) // divide the current filmstrip in half
     $('.nav-group').scrollLeft(offset)
   }
   render() {
-    return (<Well bsClass="nav-group">
-      <NavGroup data={this.state.items} currentPage={this.props.currentPage} edition={this.props.edition} />
-    </Well>
+    return (
+      <div className="nav-strip-container">
+        <NavArrow dir="prev" currentPage={this.props.currentPage} edition={this.props.edition} items={this.state.items} />
+        <Well bsClass="nav-group">
+          <NavGroup data={this.state.items} currentPage={this.props.currentPage} edition={this.props.edition} />
+        </Well>
+        <NavArrow dir="next" currentPage={this.props.currentPage} edition={this.props.edition} items={this.state.items} />
+      </div>
     )
   }
 }
@@ -44,9 +48,44 @@ NavStrip.propTypes = {
   currentPage: PropTypes.number,
 }
 
-const NavGroup = ({ data, currentPage, edition }) => (<ButtonGroup >
-  {data.map((p) => <Thumbnail pageData={p} currentPage={currentPage} edition={edition} key={p.index} />)}
-</ButtonGroup>)
+export const NavArrow = ({ items, currentPage, dir, edition }) => {
+  if (dir === 'prev' && currentPage === 1) {
+    return null
+  }
+
+  if (dir === 'next' && currentPage === items.length) {
+    return null
+  }
+
+  if (dir === 'next') {
+    return (
+      <Link to={`/reader/${edition}/${currentPage + 1}`} className="nav-strip-button next">
+        <Button bsClass="button">
+        &gt;
+        </Button>
+      </Link>
+    )
+  }
+  return (
+    <Link to={`/reader/${edition}/${currentPage - 1}`} className="nav-strip-button prev">
+      <Button bsClass="button">
+      &lt;
+      </Button>
+    </Link>
+  )
+}
+
+NavArrow.propTypes = {
+  items: PropTypes.array.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  dir: PropTypes.string.isRequired,
+  edition: PropTypes.string.isRequired,
+}
+
+const NavGroup = ({ data, currentPage, edition }) => (
+  <ButtonGroup >
+    {data.map((p) => <Thumbnail pageData={p} currentPage={currentPage} edition={edition} key={p.index} />)}
+  </ButtonGroup>)
 
 
 NavGroup.propTypes = {
