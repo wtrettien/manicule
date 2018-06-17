3eb20295bbab09c942d771efe580e9bc8ddc84b1
Liza Daly <lizadaly@gmail.com>
2018-06-17 15:42:49 -0400

    Move image URL into common function

--- app/components/page-image.js
+++ app/components/page-image.js
@@ -4,9 +4,14 @@ import PropTypes from 'prop-types'
 
 import { Image } from 'react-bootstrap'
 
-const PageImage = ({ edition, num }) => {
+export const getImageUrl = (edition, num, thumbnail = false) => {
   const pad = (`0000${num}`).substr(-4, 4)
-  const img = require(`../images/book/${edition}/${pad}.jpg`) // eslint-disable-line global-require
+  const img = thumbnail ? require(`../images/book/${edition}/thumbnails/${pad}.jpg`) : require(`../images/book/${edition}/${pad}.jpg`)
+  return img
+}
+
+const PageImage = ({ edition, num }) => {
+  const img = getImageUrl(edition, num)
   return <Image src={img} alt="" responsive />
 }
 PageImage.propTypes = {
@@ -14,3 +19,4 @@ PageImage.propTypes = {
   num: PropTypes.number.isRequired,
 }
 export default PageImage
+
