0034ba4711b7533025aaac2a97fc0468e30924eb
Liza Daly <lizadaly@gmail.com>
2018-07-21 14:09:42 -0400

    [48] Make use of loading page

--- app/containers/LoadingPage/index.js
+++ app/containers/LoadingPage/index.js
@@ -4,7 +4,7 @@ import { Jumbotron, Grid, Col, Row } from 'react-bootstrap'
 import SiteContainer from '../SiteContainer'
 
 
-export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export default class LoadingPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <SiteContainer>
@@ -15,7 +15,7 @@ export default class NotFound extends React.PureComponent { // eslint-disable-li
             <Col>
               <Jumbotron>
                 <h2>
-        Page Not Found
+        Loading...
           </h2>
               </Jumbotron>
             </Col>
