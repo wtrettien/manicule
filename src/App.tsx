34998b22267b4ad1f5f3d99c28b2d54e3f9b65e3
Liza Daly <lizadaly@gmail.com>
2021-12-07 10:09:17 -0500

    Move images folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/cc.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
