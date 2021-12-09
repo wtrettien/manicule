0ebf1b0e8fe329c9fb1a94326dbb2c7640552896
Liza Daly <lizadaly@gmail.com>
2021-12-09 14:24:51 -0500

    Limit scope of HTML import

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,11 @@
 const { loaderByName, addBeforeLoader } = require('@craco/craco')
+const path = require('path')
 
 module.exports = {
     webpack: {
         configure: (webpackConfig) => {
             webpackConfig.resolve.extensions.push('.xml')
-            webpackConfig.resolve.extensions.push('.html')
+            //webpackConfig.resolve.extensions.push('.html')
             const xmlLoader = {
                 loader: require.resolve('xml-loader'),
                 test: /\.xml$/,
@@ -13,7 +14,8 @@ module.exports = {
             const htmlLoader = {
                 loader: require.resolve('html-loader'),
                 test: /\.html$/,
-                exclude: /node_modules/
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src/data')
             }
 
             addBeforeLoader(webpackConfig, loaderByName('file-loader'), xmlLoader)
